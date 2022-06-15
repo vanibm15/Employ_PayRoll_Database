c78f05861ee7ecbef1f1bda0cc0f4f414f4c19a0
vanibm15 <vanibm15@gmail.com>
2022-06-15 20:44:01 +0530

    added:RetriveSalData

--- src/main/java/EmpRollSql.java
+++ src/main/java/EmpRollSql.java
@@ -20,11 +20,10 @@ public class EmpRollSql {
             con = DriverManager.getConnection(jdbcURL, username, password);
             System.out.println("connection done successful!!" + con);
             Statement statement = con.createStatement();
-           statement.executeUpdate("insert into employee(name,salary,department, joining_date) values ('vani',400000, 'cs-engg','2020-12-09')," +
-                    "('Praveen',50000, 'mech-engg','2018-09-06'),('Shreyas',600000, 'ee-engg','2023-11-08'),('Varsh',900000, 'IT','2001-06-07')");
-            ResultSet resultSet = statement.executeQuery("select * from employee;");
+//           statement.executeUpdate("insert into employee(name,salary,department, joining_date) values ('vani',400000, 'cs-engg','2020-12-09')," +
+//                    "('Praveen',50000, 'mech-engg','2018-09-06'),('Shreyas',600000, 'ee-engg','2023-11-08'),('Varsh',900000, 'IT','2001-06-07')");
+            ResultSet resultSet = statement.executeQuery("select * from employee where Joining_Date between '2001-06-08' and '2018-09-06'");
             while (resultSet.next())
-
             {
                 System.out.println("id:" + resultSet.getInt("id"));
                 System.out.println("name:" + resultSet.getString("name"));
