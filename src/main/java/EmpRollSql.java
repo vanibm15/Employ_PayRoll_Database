ab5d088744379de066fbfc66f3d30bd1dba29898
vanibm15 <vanibm15@gmail.com>
2022-06-16 12:14:47 +0530

    added:connectiontoJDBC

--- src/main/java/EmpRollSql.java
+++ src/main/java/EmpRollSql.java
@@ -16,18 +16,8 @@ public class EmpRollSql {
             System.out.println("connecting to database:" + jdbcURL);
             con = DriverManager.getConnection(jdbcURL, username, password);
             System.out.println("connection done successful!!" + con);
-           Statement statement=con.createStatement();
-           statement.execute("create table employee(id int auto_increment,name varchar(30),\n" +
-                   "salary double,department varchar(30),Joining_Date date,\n" +
-                   "primary key(id))");
-            ResultSet resultSet =statement.executeQuery("desc employee;");
-            while(resultSet.next()){
-                System.out.println("id:"+resultSet.getInt("id"));
-                System.out.println("name:"+resultSet.getString("name"));
-                System.out.println("salary:"+resultSet.getDouble("salary"));
-                System.out.println("department:"+resultSet.getString("department"));
-                System.out.println("Joining_Date:"+resultSet.getDate("Joining_Date"));
-            }
+
+
         } catch (SQLException ex) {
             //throw new RuntimeException(ex);
         }
